fd7bd01ad4ec587a4b9359bd6b699e1ca607dfa6
patrickpoole <patrickcpoole@gmail.com>
2024-03-25 12:31:34 -0600

    added query variable functionality

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,40 @@
 import  { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
-// creating git repo
-const server = new ApolloServer({
-  // typeDefs -- definitions of types of data author
-  // resolvers
+import db from '../_db.js'
 
-})
+import { typeDefs } from './schema.js'
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id)
+    },
+    reviews() {
+      return db.reviews
+    },
+    review(_, args) {
+     
+      return db.reviews.find((review) => review.id === args.id)
+    },
+    authors() {
+      return db.authors
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id)
+    },
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: {port: 4000}
-})
+});
 
 console.log('Server ready at port', 4000)
